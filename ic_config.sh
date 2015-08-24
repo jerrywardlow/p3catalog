1d12e497349ea340e22b43da2ed1553fcc9612da
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-08-24 11:17:20 -0700

    Removed debugging 'echo' from a few lines

--- ic_config.sh
+++ ic_config.sh
@@ -8,14 +8,11 @@ pip install requests
 pip install httplib2
 pip install flask-seasurf
 
-echo "Setting up Postgres Environment"
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb itemcatalog'
 
-echo "Running python database setup"
 su vagrant -c 'python /vagrant/database_model.py'
-echo "Python database setup complete"
 
 vagrantTip="[36m[1mTo access project files: cd /vagrant[m"
 echo -e $vagrantTip > /etc/motd
