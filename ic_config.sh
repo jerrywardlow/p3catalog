039a0f10616f521fdf9f779839cd8bcbfaff25bb
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-08-25 12:28:49 -0700

    Removed 'bleach' from pip installs

--- ic_config.sh
+++ ic_config.sh
@@ -2,7 +2,6 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
-pip install bleach
 pip install oauth2client
 pip install requests
 pip install httplib2
