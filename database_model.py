6f94552893115242b6afc6349e97212a3484be56
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-09-02 09:36:33 -0700

    Removed unnecessary import of backref

--- database_model.py
+++ database_model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from datetime import datetime
 
