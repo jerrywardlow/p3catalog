a05ea5cce66f5e4115ca840fb26e522960265085
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-09-09 12:39:43 -0700

    Initial Commit

---
+++ imgur_uploader.py
@@ -0,0 +1,14 @@
+from flask import request
+from base64 import b64encode
+import pyimgur
+
+IMGUR_CLIENT_ID = '873ad6bb61b29ec'
+
+def new_upload(target_file):
+    '''Reads and encodes the target file before uploading to Imgur'''
+    data = b64encode(target_file.read())
+    client = pyimgur.Imgur(IMGUR_CLIENT_ID)
+    image = client._send_request('https://api.imgur.com/3/image',
+                             method='POST',
+                             params={'image': data})
+    return image
