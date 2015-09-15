5c77f9f79641e1657e3740968c6d842bb4ea6d03
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-09-15 15:47:09 -0700

    Moved CLIENT_ID to variable to allow multiple options later

--- imgur_uploader.py
+++ imgur_uploader.py
@@ -2,12 +2,10 @@ from flask import request
 from base64 import b64encode
 import pyimgur
 
-from website_config import IMGUR_CLIENT_ID
-
-def imgur_upload(target_file):
+def imgur_upload(target_file, client_id):
     '''Reads and encodes the target file before uploading to Imgur'''
     data = b64encode(target_file.read())
-    client = pyimgur.Imgur(IMGUR_CLIENT_ID)
+    client = pyimgur.Imgur(client_id)
     image = client._send_request('https://api.imgur.com/3/image',
                              method='POST',
                              params={'image': data})
