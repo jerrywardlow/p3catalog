0054f6aa8c2caa6878d31d5393f5693815c11b69
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-09-15 15:26:13 -0700

    Changed DEBUG status

--- website_config.py
+++ website_config.py
@@ -1,7 +1,7 @@
 import json
 
 SECRET_KEY = 'super_super_secret_secret_key'
-DEBUG = True
+DEBUG = False
 CLIENT_SECRETS = 'client_secrets.json'
 CLIENT_ID = json.loads(open(CLIENT_SECRETS, 'r').read())['web']['client_id']
 
