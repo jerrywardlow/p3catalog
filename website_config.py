0cd8b78083464eb63f942cd4bcc5cdae4f59a529
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-13 15:07:05 -0700

    Formatting

--- website_config.py
+++ website_config.py
@@ -1,5 +1,7 @@
 import json
 
+IMGUR_CLIENT_ID = 'PLACE IMGUR CLIENT ID HERE'
+
 SECRET_KEY = 'super_super_secret_secret_key'
 DEBUG = False
 CLIENT_SECRETS = 'client_secrets.json'
@@ -7,5 +9,3 @@ try:
     CLIENT_ID = json.loads(open(CLIENT_SECRETS, 'r').read())['web']['client_id']
 except:
     CLIENT_ID = False
-
-IMGUR_CLIENT_ID = 'PLACE IMGUR CLIENT ID HERE'
