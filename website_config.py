4f207b4bf78dffb3a08acf1b45d07b9d23e88ce0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-13 15:03:54 -0700

    Catch exception when loading CLIENT_ID from JSON

--- website_config.py
+++ website_config.py
@@ -3,6 +3,9 @@ import json
 SECRET_KEY = 'super_super_secret_secret_key'
 DEBUG = False
 CLIENT_SECRETS = 'client_secrets.json'
-CLIENT_ID = json.loads(open(CLIENT_SECRETS, 'r').read())['web']['client_id']
+try:
+    CLIENT_ID = json.loads(open(CLIENT_SECRETS, 'r').read())['web']['client_id']
+except:
+    CLIENT_ID = False
 
 IMGUR_CLIENT_ID = 'PLACE IMGUR CLIENT ID HERE'
