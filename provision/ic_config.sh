4fc7a5ec69e20b8aee84188819fc614dc23156c5
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-14 10:23:34 -0700

    Commenting

--- provision/ic_config.sh
+++ provision/ic_config.sh
@@ -1,20 +1,28 @@
+# Update apt cache
 apt-get -qqy update
+# Install project packages
 apt-get -qqy install postgresql python-psycopg2 python-pip
 
+# Install Python packages
 pip install -r /itemcatalog/requirements.txt
 pip install gunicorn
 
+# Create new PostgreSQL user and databases
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb itemcatalog'
 
+# Populate databases with catalog model and sample data
 su vagrant -c 'python /itemcatalog/database_model.py'
 su vagrant -c 'python /itemcatalog/populator.py'
 
+# Copy upstart script and start Gunicorn
 cp /itemcatalog/itemcatalog.conf /etc/init/
 start itemcatalog
 
+# Automatically change to project directory on login
 echo 'cd /itemcatalog' > /home/vagrant/.bash_profile
 
+# Change the message of the day
 vagrantTip="[36m[1mItem Catalog running on 'localhost:5000'[m"
 echo -e $vagrantTip > /etc/motd
