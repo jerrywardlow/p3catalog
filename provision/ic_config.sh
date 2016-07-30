3a898c529c238bd4ffc6423fbe5ccd320c78a184
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-29 23:24:55 -0700

    Append to .bash_profile

--- provision/ic_config.sh
+++ provision/ic_config.sh
@@ -20,7 +20,7 @@ cp /itemcatalog/itemcatalog.conf /etc/init/
 start itemcatalog
 
 # Automatically change to project directory on login
-echo 'cd /itemcatalog' > /home/vagrant/.bash_profile
+echo 'cd /itemcatalog' >> /home/vagrant/.bash_profile
 
 # Change the message of the day
 vagrantTip="[36m[1mItem Catalog running on 'localhost:5000'[m"
