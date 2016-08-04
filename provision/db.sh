613a8611fd659f57ed496a2edd25aa00fd3dbd0f
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-04 13:55:44 -0700

    Load sample data into database

--- provision/db.sh
+++ provision/db.sh
@@ -7,6 +7,9 @@ apt-get -qqy install postgresql
 su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
 su postgres -c 'createdb itemcatalog'
 
+# Load sample data into database
+su postgres -c 'psql itemcatalog < sample_catalog.sql'
+
 # Configure PostgreSQL to accept remote connections
 echo 'host all all 192.168.56.3/32 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
 echo "listen_addresses='192.168.56.2'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
