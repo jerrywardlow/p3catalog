4ca986895ff1fbfa572240e5f12b829d5d73e466
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-04 13:34:17 -0700

    Provisioner for postgres database node

---
+++ provision/db.sh
@@ -0,0 +1,15 @@
+# Update apt cache
+apt-get -qqy update
+# Install packages
+apt-get -qqy install postgresql
+
+# Create new database user and database
+su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
+su postgres -c 'createdb itemcatalog'
+
+# Configure PostgreSQL to accept remote connections
+echo 'host all all 192.168.56.3/32 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
+echo "listen_addresses='192.168.56.2'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
+
+# Restart the PostgreSQL service
+service postgresql restart
