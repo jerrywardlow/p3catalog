76c236c7ccfa33b9708aba77cd7776e51fa6fa68
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-04 13:53:26 -0700

    Remove database related items

--- provision/ic_config.sh
+++ provision/ic_config.sh
@@ -7,15 +7,6 @@ apt-get -qqy install python-psycopg2 \
 # Install Python packages
 pip install -r /itemcatalog/requirements.txt
 
-# Create new PostgreSQL user and databases
-su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb itemcatalog'
-
-# Populate databases with catalog model and sample data
-su vagrant -c 'python /itemcatalog/database_model.py'
-su vagrant -c 'python /itemcatalog/populator.py'
-
 # Copy upstart script and start Gunicorn
 cp /itemcatalog/itemcatalog.conf /etc/init/
 start itemcatalog
