46afe9a3ba90816bd7a30087e4b2199704d804d0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-17 12:39:46 -0700

    Renamed sample_catalog to reflect db type

--- provision/shell/db.sh
+++ provision/shell/db.sh
@@ -8,7 +8,7 @@ su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
 su postgres -c 'createdb itemcatalog'
 
 # Load sample data into database
-su postgres -c 'psql itemcatalog < /db/sample_catalog.sql'
+su postgres -c 'psql itemcatalog < /db/postgres.sample_catalog.sql'
 
 # Configure PostgreSQL to accept remote connections
 echo 'host all all 192.168.56.3/32 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
