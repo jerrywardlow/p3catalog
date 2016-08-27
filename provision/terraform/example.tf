931770f87de8bb81bf747648a3c17cf8b80eb449
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-27 07:28:35 -0700

    Add RHEL server

--- provision/terraform/example.tf
+++ provision/terraform/example.tf
@@ -7,6 +7,11 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
 }
 
+resource "aws_instance" "rhel" {
+    ami = "ami-775e4f16"
+    instance_type = "t2.micro"
+}
+
 resource "aws_eip" "ip" {
     instance = "${aws_instance.example.id}"
 }
