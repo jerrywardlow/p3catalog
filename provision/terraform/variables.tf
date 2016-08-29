f5cfbae510637da4e1379ee9d38bb95b75be54b2
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-29 11:24:04 -0700

    Add AMI variables

--- provision/terraform/variables.tf
+++ provision/terraform/variables.tf
@@ -3,3 +3,12 @@ variable "secret_key" {}
 variable "region" {
   default = "us-west-2"
 }
+
+variable "amis" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-81365496"
+    us-west-1 = "ami-05105d65"
+    us-west-2 = "ami-9ece19fe"
+  }
+}
