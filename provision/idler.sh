f868bad0d034cac5d405c405e308d25e971b4134
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-08-29 18:34:48 -0700

    Correctly access postgres, general smoothing

--- provision/idler.sh
+++ provision/idler.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-cmd="$1"
-
-until psql -h "postgres" -U "postgres" -c '\l'; do
+until psql -h "postgres" -U "pgdbuser" -d "itemcatalog" -c '\l'; do
     >&2 echo "Waiting on postgres..."
     sleep 1
 done
 
 >&2 echo "Postgres up, gun time!"
-exec cmd
+gunicorn -b 0.0.0.0:5000 wsgi:app
