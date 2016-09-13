cb05e76c8c692107c20eea1f021bc320f857f133
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-13 16:46:38 -0700

    Use constant for query

--- rework/catalog/views.py
+++ rework/catalog/views.py
@@ -4,21 +4,23 @@ from catalog import app, db
 
 from .models import User, Category, Item
 
+CATEGORIES_QUERY = Category.query.order_by(asc(Category.name))
+
 @app.route('/')
 @app.route('/index')
 def index():
-    categories = Category.query.order_by(asc(Category.name))
+    categories = CATEGORIES_QUERY
     items = Item.query.order_by(desc(Item.created)).limit(4).all()
     pass
 
 @app.route('/categories/')
 def categories():
-    categories = Category.query.order_by(asc(Category.name))
+    categories = CATEGORIES_QUERY
     pass
 
 @app.route('/category/<int:category_id>/')
 def category(category_id):
-    categories = Category.query.order_by(asc(Category.name))
+    categories = CATEGORIES_QUERY
     category = Category.query.filter_by(id=category_id).first()
     items = Item.query.filter_by(category_id=category_id).all()
     pass
