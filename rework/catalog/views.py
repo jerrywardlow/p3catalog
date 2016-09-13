e3b8ee264f88a38c4b9eec1ab0ef27c4395ef4a5
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-13 16:45:44 -0700

    Views for all categories and single category

--- rework/catalog/views.py
+++ rework/catalog/views.py
@@ -10,3 +10,15 @@ def index():
     categories = Category.query.order_by(asc(Category.name))
     items = Item.query.order_by(desc(Item.created)).limit(4).all()
     pass
+
+@app.route('/categories/')
+def categories():
+    categories = Category.query.order_by(asc(Category.name))
+    pass
+
+@app.route('/category/<int:category_id>/')
+def category(category_id):
+    categories = Category.query.order_by(asc(Category.name))
+    category = Category.query.filter_by(id=category_id).first()
+    items = Item.query.filter_by(category_id=category_id).all()
+    pass
