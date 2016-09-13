be3f5d2d97091e34f832f745ae26f8e52fce5fb1
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-12 17:34:31 -0700

    Placeholder for 'user' model

--- rework/catalog/models.py
+++ rework/catalog/models.py
@@ -2,6 +2,11 @@ from . import db
 
 from datetime import datetime
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
 class Category(db.Model):
     __tablename__ = 'categories'
 
