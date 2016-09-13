ee450300d777658d53b6121a5f97f5c2a87d5d82
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-13 16:40:56 -0700

    Queries for index

--- rework/catalog/views.py
+++ rework/catalog/views.py
@@ -3,3 +3,10 @@ from flask import render_template
 from catalog import app, db
 
 from .models import User, Category, Item
+
+@app.route('/')
+@app.route('/index')
+def index():
+    categories = Category.query.order_by(asc(Category.name))
+    items = Item.query.order_by(desc(Item.created)).limit(4).all()
+    pass
