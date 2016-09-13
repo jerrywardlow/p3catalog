d4e5a4234e599e7c17b1fdaacb17cd0b8029bd19
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-12 17:32:36 -0700

    Add model for 'item'

--- rework/catalog/models.py
+++ rework/catalog/models.py
@@ -12,3 +12,17 @@ class Category(db.Model):
     created = db.Column(db.DateTime, default = datetime.now)
     user_id = db.Column(db.Integer, ForeignKey('users.id'))
     user = db.relationship(User)
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    photo = db.Column(db.String)
+    created = db.Column(db.DateTime, default = datetime.now)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id',
+                                                      ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship(User)
+    category = db.relationship(Category)
