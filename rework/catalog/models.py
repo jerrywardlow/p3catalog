549538d9355496ebf48323fc8a9e6a7f5a178903
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-12 17:24:47 -0700

    Add model for 'category'

--- rework/catalog/models.py
+++ rework/catalog/models.py
@@ -1,1 +1,12 @@
 from . import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    photo = db.Column(db.String)
+    created = db.Column(db.DateTime, default = datetime.now)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    user = db.relationship(User)
