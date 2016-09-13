bde4f35c391379e793ea0b8e1fc2945c44b2a2bc
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-12 17:51:06 -0700

    Use Flask-Login

--- rework/catalog/__init__.py
+++ rework/catalog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,5 +8,9 @@ app.config.from_pyfile('config.py')
 
 # Flask-SQLAlchemy
 db = SQLAlchemy(app)
+
+# Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 import catalog.models
