3d75e40dbb683c7dbfd9bf2ad093a5689612aacf
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-20 18:05:00 -0700

    Add fields to User model for testing against sample database

--- rework/catalog/models.py
+++ rework/catalog/models.py
@@ -8,6 +8,9 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    picture = db.Column(db.String)
 
 class Category(db.Model):
     __tablename__ = 'categories'
