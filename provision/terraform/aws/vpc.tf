6dbce6ea298dec21cf20c58da4403f2364792af1
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-02 21:00:46 -0700

    Configure VPC

--- provision/terraform/aws/vpc.tf
+++ provision/terraform/aws/vpc.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-    
+    region = "us-west-2"    
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    tags {
+        Name = "item-catalog"
+    }
 }
