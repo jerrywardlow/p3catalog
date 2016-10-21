6b7bf55d736aeb8f9c65155554f0808042026c47
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-21 16:44:39 -0700

    Subnet CIDR block variables for private and public

--- provision/terraform/aws/variables.tf
+++ provision/terraform/aws/variables.tf
@@ -2,3 +2,13 @@ variable "region" {
     description = "AWS Region"
     default = "us-west-2"
 }
+
+variable "public_subnet_cidr" {
+    description = "CIDR block for public subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR block for private subnet"
+    default = "10.0.10.0/24"
+}
