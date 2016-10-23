c7eb12221e007b092f6f12b3ac83a64e8c0e3620
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-22 23:44:11 -0700

    Ingress rule for SSH

--- provision/terraform/aws/security-groups.tf
+++ provision/terraform/aws/security-groups.tf
@@ -4,5 +4,10 @@ resource "aws_security_group" "nat" {
     description = "Security group for NAT instance"
     vpc_id = "${aws_vpc.default.id}"
 
-
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
