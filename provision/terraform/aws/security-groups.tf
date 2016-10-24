06fbd780210009a8d182d197e6e6414ffbc21345
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-10-23 20:53:41 -0700

    Ingress rule for port 80 from private subnet

--- provision/terraform/aws/security-groups.tf
+++ provision/terraform/aws/security-groups.tf
@@ -10,4 +10,11 @@ resource "aws_security_group" "nat" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
 }
